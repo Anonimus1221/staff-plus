6464a32971f8ae30cf8b2f56a07901b515807524
Anonimus1223 <122642283+Anonimus1221@users.noreply.github.com>
2026-04-27 22:31:40 -0500

    problem solving

--- StaffPlusCore/src/main/java/net/shortninja/staffplus/server/listener/player/PlayerQuit.java
+++ StaffPlusCore/src/main/java/net/shortninja/staffplus/server/listener/player/PlayerQuit.java
@@ -2,6 +2,7 @@ package net.shortninja.staffplus.server.listener.player;
 
 import net.shortninja.staffplus.StaffPlus;
 import net.shortninja.staffplus.player.UserManager;
+import net.shortninja.staffplus.player.attribute.InventorySerializer;
 import net.shortninja.staffplus.player.attribute.TicketHandler;
 import net.shortninja.staffplus.player.attribute.mode.ModeCoordinator;
 import net.shortninja.staffplus.player.attribute.mode.handler.VanishHandler;
@@ -16,6 +17,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
 
 public class PlayerQuit implements Listener {
     private MessageCoordinator message = StaffPlus.get().message;
@@ -36,16 +40,31 @@ public class PlayerQuit implements Listener {
 
         Player player = event.getPlayer();
         manageUser(player);
+
+        // Si el jugador está en staff mode, guardar su inventario actual antes de
+        // removerlo
+        if (modeCoordinator.isInMode(player.getUniqueId())) {
+            InventorySerializer inventorySaver = new InventorySerializer(player.getUniqueId());
+            HashMap<Integer, ItemStack> items = new HashMap<>();
+            for (int i = 0; i <= 35; i++) {
+                if (player.getInventory().getItem(i) != null) {
+                    items.put(i, player.getInventory().getItem(i));
+                }
+            }
+            inventorySaver.save(items, player.getInventory().getArmorContents(), player.getExp());
+        }
+
         modeCoordinator.removeMode(player);
         vanishHandler.removeVanish(player);
-        ticketHandler.removeTicket(ticketHandler.getTicketByUuid(player.getUniqueId()), "", TicketHandler.TicketCloseReason.QUIT);
+        ticketHandler.removeTicket(ticketHandler.getTicketByUuid(player.getUniqueId()), "",
+                TicketHandler.TicketCloseReason.QUIT);
         if (userManager.get(player.getUniqueId()).isFrozen()) {
             for (String command : options.logoutCommands) {
                 command = command.replace("%player%", player.getName());
                 Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command);
             }
         }
-        if(options.enderOfflineChestEnabled && !InventoryFactory.isInventoryEmpty(event.getPlayer().getEnderChest())){
+        if (options.enderOfflineChestEnabled && !InventoryFactory.isInventoryEmpty(event.getPlayer().getEnderChest())) {
             InventoryFactory.saveEnderChest(event.getPlayer());
         }
     }
@@ -56,7 +75,8 @@ public class PlayerQuit implements Listener {
         user.setOnline(false);
 
         if (user.isFrozen()) {
-            message.sendGroupMessage(messages.freezeLogout.replace("%player%", player.getName()), options.permissionFreeze, messages.prefixGeneral);
+            message.sendGroupMessage(messages.freezeLogout.replace("%player%", player.getName()),
+                    options.permissionFreeze, messages.prefixGeneral);
         }
     }
 }
